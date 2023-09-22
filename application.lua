92f904947b32699bd1939a104064ec2a2d11f3fd
timsa <31769013+541263@users.noreply.github.com>
2023-09-22 18:20:37 +0500

    Update application.lua

--- application.lua
+++ application.lua
@@ -1,6 +1,6 @@
-local host = ""
-local user = ""
-local pass = ""
+local mqtt_host = ""
+local mqtt_user = ""
+local mqtt_pass = ""
 
 -- 0	GPIO16
 -- 1	GPIO5
@@ -16,6 +16,8 @@ local fan_level1_pin = 7		-- 7	GPIO13
 local water_valve_pin = 8		-- 8	GPIO15
 local drain_pump_pin = 12		-- 12	GPIO10
 
+local power_state = 0			-- 0 off, 1 on, 2 standby
+
 local air_in_temp = 0
 local air_out_temp = 0
 local air_delta = 0
@@ -28,7 +30,7 @@ local water_valve = 0
 local drain_pump = 0
 local fan_level = 0
 
-function initpins()
+function init_pins()
 	gpio.mode(drain_pump_pin, gpio.OUTPUT)
 	gpio.mode(water_valve_pin, gpio.OUTPUT)
 	gpio.mode(fan_level1_pin, gpio.OUTPUT)
@@ -52,7 +54,7 @@ function drain_pump_off()
 	gpio.write(drain_pump_pin, gpio.LOW)
 end
 
-function fan_level(level=0)
+function fan(level=0)
 	if level == 1 then
 		gpio.write(fan_level1_pin, gpio.HIGH)
 		gpio.write(fan_level2_pin, gpio.LOW)
@@ -72,46 +74,71 @@ function fan_level(level=0)
 	end
 end
 
+function connect_mqtt()
+    m:connect(mqtt_host, 1883, false, function(client)
+		print("Connected")
+		client:publish("sm/state", "online", 0, 0)
+		client:subscribe("sm/state", 0)
+    end,
+	function(client, reason)
+		print("Connection failed reason: " .. reason)
+	end)
+end
 
+function drain_pump_loop()
+	if power_state > 0 then
+		if drain_level > 0 then
+			drain_pump_on()
+		else
+			drain_pump_off()
+		end
+	else
+		drain_pump_off()
+	end
+end
 
+function mqtt_send_loop()
+	m:publish("conditioner/air_inlet", air_in_temp, 0, 0)
+	if fan_level > 0 then
+		m:publish("conditioner/air_outlet", air_out_temp, 0, 0)
+	end
+	if water_valve_pin == 1 then
+	    m:publish("conditioner/water_inlet", water_in_temp, 0, 0)
+	    m:publish("conditioner/water_outlet", water_out_temp, 0, 0)
+	end
+end
 
+function main_loop()
+	
+end
 
-m = mqtt.Client("conditioner", 120, user, pass)
+m = mqtt.Client("conditioner", 120, mqtt_user, mqtt_pass)
 
 m:lwt("/lwt", "offline", 0, 0)
 
 m:on("offline", function(client) -- restart connection in any case
-		m:close()
-		print ("Disconnected")
-        tmr.create():alarm(2000, tmr.ALARM_SINGLE, connect) -- in a couple of seconds
-	end)
+	m:close()
+	print ("Disconnected")
+	tmr.create():alarm(2000, tmr.ALARM_SINGLE, connect_mqtt) -- in a couple of seconds
+end)
 
 m:on("message", function(client, topic, data)
-		if topic == "sm/state" then
-		    if data == "on" then
-			      commandOn()
+	if topic == "conditioner/state" then
+	    if data == "on" then
+		    power_state = 1
         elseif data == "off" then
-			      commandOff()
-        end
-		end
-	end)
+			power_state = 0
+		else
+			power_state = 2
+    	end
+	elseif topic == "conditioner/set_temp" then
+		air_set_temp = data
+	end
+end)
 
-function connect()
-    m:connect(host, 1883, false, function(client)
-            print("Connected")
-            client:publish("sm/state", "online", 0, 0)
-            client:subscribe("sm/state", 0)
-        end,
-        function(client, reason)
-            print("Connection failed reason: " .. reason)
-        end)
-end
+init_pins()
+connect_mqtt()
 
-function loop()
-    -- collect some data and send it
-    m:publish("sm/temp", "20.5", 0, 0)
-end
-
-connect()
-
-tmr.create():alarm(60000, tmr.ALARM_AUTO, loop) -- every 60 seconds
+tmr.create():alarm(60000, tmr.ALARM_AUTO, mqtt_send_loop) 	-- every 60 seconds
+tmr.create():alarm(30000, tmr.ALARM_AUTO, main_loop) 		-- every 30 seconds
+tmr.create():alarm(3000, tmr.ALARM_AUTO, drain_pump_loop)	-- every 3 seconds
